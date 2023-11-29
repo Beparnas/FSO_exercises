93824294612bcdc30da80b006085247204d7b897
beparnas <beparnas@gmail.com>
2023-11-29 13:20:37 -0500

    part 1.1 complete

---
+++ Part1/part1.1/src/App.jsx
@@ -0,0 +1,50 @@
+import { Component } from "react"
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const sections = [[part1,exercises1],[part2,exercises2],[part3,exercises3]]
+  return (
+    <div>
+      <Header course={course}/>
+      <Content sections={sections} />
+      <Totals sections={sections}/>
+    </div>
+  )
+}
+
+const Header = (props)=>{
+  return(
+  <>
+    <h1>{props.course}</h1>
+  </>
+  )
+}
+
+const Content = (props)=>{
+  console.log("content: "+props.sections)
+  return(
+    <>
+      {props.sections.map(course=>(
+        <p key={course[0]}>{course[0]} {course[1]}</p> // key added as best practice
+      ))
+      }
+    </>
+  )
+}
+
+const Totals = (props)=>{
+  var totalCount = 0
+  props.sections.forEach(element => {
+    totalCount+=element[1]
+  })
+  return(
+    <p>Number of exercises {totalCount}</p>
+  )
+}
+export default App

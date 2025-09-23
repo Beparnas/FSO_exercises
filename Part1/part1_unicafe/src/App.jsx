593f7bef7e721f459c633a90c616f787adce2fd4
beparnas <beparnas@gmail.com>
2025-09-23 15:56:18 -0700

    1.6 complete

--- Part1/part1_unicafe/src/App.jsx
+++ Part1/part1_unicafe/src/App.jsx
@@ -11,23 +11,31 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  //statistical data
+  const [avg, updateAvg] = useState(0)
 
   //event handlers
   const handleGood = () => () => {
     setGood(good+1)
+    console.log("good is now:",good+1)
   }
   const handleBad = () => () => {
     setBad(bad+1)
+    console.log("bad is now:", bad+1)
   }
   const handleNeutral = () => () => {
     setNeutral(neutral+1)
+    console.log("neutral is now:",neutral+1)
   }
   return (
     <div>
-      <Header value="Feedback"/>
+      <Header value="Give Feedback"/>
       <button onClick={handleGood()}>good</button>
       <button onClick={handleBad()}>bad</button>
       <button onClick={handleNeutral()}>neutral</button>
+      <Header value="Statistics"/>
+      <p>good: {good} <br></br> bad: {bad}<br></br> neutral: {neutral}</p>
+
     </div>
   )
 }

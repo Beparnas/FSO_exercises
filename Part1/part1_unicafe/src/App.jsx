1c04e01ebd5c80ed11d3f9de637b0929bfebaada
beparnas <beparnas@gmail.com>
2025-09-23 12:30:06 -0400

    partial completion of exercise 1.6

---
+++ Part1/part1_unicafe/src/App.jsx
@@ -0,0 +1,37 @@
+//exercise 1.6: 
+import { useState } from 'react'
+const Header = (props) =>{
+  return (
+  <>
+    <h1>{props.value}</h1>
+  </>)
+}
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  //event handlers
+  const handleGood = () => () => {
+    setGood(good+1)
+  }
+  const handleBad = () => () => {
+    setBad(bad+1)
+  }
+  const handleNeutral = () => () => {
+    setNeutral(neutral+1)
+  }
+  return (
+    <div>
+      <Header value="Feedback"/>
+      <button onClick={handleGood()}>good</button>
+      <button onClick={handleBad()}>bad</button>
+      <button onClick={handleNeutral()}>neutral</button>
+    </div>
+  )
+}
+
+
+
+export default App

c38c842dc66c215ebeabc038e1fc675c264692c0
beparnas <beparnas@gmail.com>
2025-10-03 16:30:55 -0700

    partial commit of refactor changes

---
+++ Part1/courseinfo/src/App.jsx
@@ -0,0 +1,69 @@
+//exercise 1.4: done, I got ahead of myself!
+
+import { Component } from "react"
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  const sections = course.parts
+  return (
+    <div>
+      <Header course={course}/>
+      <Content sections={sections} />
+      <Totals sections={sections}/>
+    </div>
+  )
+}
+
+const Header = (props)=>{
+  return(
+  <>
+    <h1>{props.course.name}</h1>
+  </>
+  )
+}
+
+const Content = (props)=>{
+  console.log("content: "+props.sections)
+  return(
+    <>
+      {props.sections.map(section=>(
+        <Section section={section} key={section.name}/>// key added as best practice
+      ))
+      }
+    </>
+  )
+}
+const Section = (props)=>{
+  const section = props.section
+  return(
+    <p><span style={{fontWeight: "bold"}}>section:</span> {section.name} <span style={{fontWeight: "bold"}}>num exercises:</span> {section.exercises}</p> 
+  )
+}
+
+const Totals = (props)=>{
+  let totalCount = 0
+  props.sections.forEach(element => {
+    totalCount+=element.exercises
+  })
+  return(
+    <p><span style={{fontWeight: "bold"}}>Total Number of exercises:</span> {totalCount}</p>
+  )
+}
+export default App
